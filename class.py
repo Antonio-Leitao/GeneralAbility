301cf814dfc38097e621c0768c2fa2dc8ad4116f
nalpalhao <nuno.alpalhao96@gmail.com>
2020-07-22 16:57:45 +0100

    test

--- class.py
+++ class.py
@@ -1,7 +1,10 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import tensorflow.keras as K
+import tensorflow.keras.backend as K
 
 from scipy.spatial import distance_matrix
 from sklearn.preprocessing import StandardScaler
@@ -23,6 +26,75 @@ def model_create(shape, loss, metrics, X_shape):
     return model
 
 
+def custom_loss_1(train, d_matrix):
+    def loss(y_true, y_pred):
+        # X_train = train[0]
+
+        dist = K.equal(y_true, d_matrix[:, -1])
+        b = tf.boolean_mask(d_matrix[:, :-1], dist)
+
+        k = y_pred.shape[0]
+
+        if not k:
+            k=1
+
+        distances = K.repeat_elements(K.flatten(b), rep=k, axis=0)
+
+        distances = K.cast(distances, dtype='float32')
+
+        print(y_pred.shape)
+        print(K.repeat_elements(K.abs(y_true - y_pred), rep=k, axis=0).shape)
+        print(K.concatenate([K.abs(y_true - y_pred)] * k, axis=-1).shape)
+        print(distances.shape)
+        print('\n')
+
+        estimation = K.exp(-(K.repeat_elements(K.abs(y_true - y_pred), rep=k, axis=0) - K.concatenate(
+            [K.abs(y_true - y_pred)] * k, axis=-1)) / (1 + distances))
+
+        score = K.reshape(estimation, shape=(k, k))
+
+        top = K.mean(score, axis=1)
+
+        mul = K.abs(y_true - y_pred) * top
+
+        return K.mean(mul)
+    return loss
+
+def custom_loss_2(train, d_matrix):
+    def loss(y_true, y_pred):
+        X_train = train[0]
+        n = K.shape(y_pred)[0:1]
+        k = len(X_train)
+        print(n, k)
+
+        NN = np.array([np.argsort(d_matrix[:, i], axis=0)[:k] for i in range(d_matrix.shape[1])])
+
+        estimation = (K.repeat_elements(K.abs(y_true-y_pred), rep=k, axis=0)-K.concatenate([K.abs(y_true-y_pred)] * n, axis=-1))
+
+        print(estimation)
+
+        p_x = K.mean(K.exp(-estimation))
+
+        K.mean(K.abs(y_pred - y_true)*p_x)
+
+        e = [K.mean([K.exp(
+            -np.divide(K.abs(K.abs(y_pred[i] - y_true[i]) - K.abs(y_pred[j] - y_true[j])),
+                       d_matrix[j, i] + 1)) for j in NN[i]]) for i in range(y_true.shape[1])]
+        M = K.abs(y_pred - y_true) * e
+        score = K.mean(M)
+
+        return score
+
+    return loss
+
+
+def custom_loss_2(d_matrix):
+    def loss(y_true, y_pred):
+        return K.mean(K.square(y_pred - y_true) + K.square(), axis=-1)
+
+    return loss
+
+
 class Generalization(tf.keras.callbacks.Callback):
 
     def __init__(self, train, test, d_matrix):
@@ -33,7 +105,7 @@ class Generalization(tf.keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs={}):
         logs['gen_score'] = float('-inf')
-        logs['n_diff'] = float('-inf')
+
         X_train, y_train = self.train[0], self.train[1]
         X_test, y_test = self.test[0], self.test[1]
 
@@ -41,9 +113,7 @@ class Generalization(tf.keras.callbacks.Callback):
         y_test_pred = self.model.predict(X_test)
 
         k = len(y_train_pred) - 1
-        # C = np.min(self.dist, axis=0)
-        C = np.array([np.mean(self.dist[:, i][np.argsort(self.dist[:, i], axis=0)][:k], axis=0) for i in
-                      range(self.dist.shape[1])])
+
         NN = np.array([np.argsort(self.dist[:, i], axis=0)[:k] for i in range(self.dist.shape[1])])
 
         e = [np.mean([np.exp(
@@ -54,7 +124,7 @@ class Generalization(tf.keras.callbacks.Callback):
         logs['gen_score'] = np.round(score, 5)
 
 
-class GEN_NN_benchmark_:
+class GEN_NN_benchmark:
     def __init__(self, model_function, model_shape, loss_function, metrics, callback):
         self.model_shape = model_shape
         self.loss = loss_function
@@ -73,9 +143,9 @@ class GEN_NN_benchmark_:
         self.X_test = StandardScaler().fit_transform(self.X_test)
         self.y_test = StandardScaler().fit_transform(self.y_test.reshape(-1, 1))
 
-        self.d_matrix = distance_matrix(self.X_train, self.X_test)
+        self.d_matrix = np.c_[distance_matrix(self.X_train, self.X_train), self.y_train]
 
-        self.model = self.model_function(self.model_shape, self.loss, self.metric, (self.X_train.shape[1],))
+        self.model = self.model_function(self.model_shape, self.loss(self.X_train, self.d_matrix), self.metric, (self.X_train.shape[1],))
 
         self.callback = self.callback(train=(self.X_train, self.y_train), test=(self.X_test, self.y_test), d_matrix=self.d_matrix)
 
@@ -96,15 +166,13 @@ class GEN_NN_benchmark_:
 
                 for seed in seeds:
                     self.build(X, y, .33, seed)
-                    history = self.model.fit(self.X_train, self.y_train, validation_data=[self.X_test, self.y_test],
-                                             epochs=epochs, batch_size=16, verbose=0, callbacks=[self.callback])
+                    history = self.model.fit(self.X_train, self.y_train, validation_data=(self.X_test, self.y_test),
+                                             epochs=epochs, batch_size=16, verbose=1, callbacks=[self.callback])
                     self.results.append([seed, history.history])
 
                     return self.results
 
 
-test = GEN_NN_benchmark_(model_create, [[1, 'relu'], [1, 'linear']], 'mse', ['mae'], Generalization)
+test = GEN_NN_benchmark(model_create, [[10, 'relu']*5, [1, 'linear']], custom_loss_1, ['mae'], Generalization)
 
 t = test.benchmark([1], 20, ['BIOAVAILABILITY'])
-
-print(t)
