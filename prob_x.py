6b96a9fd64393881eb30c7cdf00e21b892502a36
Antonio-Leitao <58305445+Antonio-Leitao@users.noreply.github.com>
2020-07-23 12:15:56 +0100

    Create prob_x.py

---
+++ prob_x.py
@@ -0,0 +1,15 @@
+from sklearn.neighbors import KernelDensity
+import numpy as np
+from sklearn.neighbors import KDTree
+
+
+def get_proba(X,n_neighbors=15):
+  tree = KDTree(X)         
+  D, idx = tree.query(X, k=n_neighbors)
+  P = np.zeros([len(X), len(X)])
+  for i in range(len(X)):
+    kde = KernelDensity().fit(X[idx[i]])
+    probs = np.exp(kde.score_samples(X))
+    P[i,:] = probs
+  
+  return np.sum(P,axis=1)/np.max(np.sum(P,axis=1))
